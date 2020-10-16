0e4bbaec565bb0160b820fe7ab39a2bd867b7216
Jonathan <jonathan.aldana170500@gmail.com>
2020-10-15 23:39:48 -0500

    Full Marca de numeros

--- src/relojanalogico/RelojAnalogico.java
+++ src/relojanalogico/RelojAnalogico.java
@@ -41,7 +41,7 @@ public class RelojAnalogico extends JFrame {
     public void paint(Graphics grphcs) {
         super.paint(grphcs); //To change body of generated methods, choose Tools | Templates.
         ciruclo(grphcs);
-        fullMarcaSegundero(grphcs);
+        fullMarcas(grphcs);
         movimientoSegundero(grphcs);
     }
 
@@ -50,15 +50,54 @@ public class RelojAnalogico extends JFrame {
         int x = this.getWidth() / 4;
         int y = this.getHeight() / 4;
         g.fillOval(x, y, 2 * x, 2 * y);
+    }
+
+    public void fullMarcas(Graphics g) {
+        for (int i = 0; i < 360; i += 6) {
+            marcaSegundero(g, i);
+        }
+        for (int i = 0; i < 360; i += 30) {
+            marcaHora(g, i);
+        }
+        fullMarcaNumeros(g);
 
     }
 
-    public void fullMarcaSegundero(Graphics g) {
-        for (int i = 0; i < 360; i += 6) {
-            
-            marcaSegundero(g, i);
+    public void fullMarcaNumeros(Graphics g) {
+        marcaNumero(g, 0, "3");
+        marcaNumero(g, 30, "4");
+        marcaNumero(g, 60, "5");
+        marcaNumero(g, 90, "6");
+        marcaNumero(g, 120, "7");
+        marcaNumero(g, 150, "8");
+        marcaNumero(g, 180, "9");
+        marcaNumero(g, 210, "10");
+        marcaNumero(g, 240, "11");
+        marcaNumero(g, 270, "12");
+        marcaNumero(g, 300, "1");
+        marcaNumero(g, 330, "2");
+    }
 
-        }
+    public void marcaNumero(Graphics g, int angulo, String hora) {
+        Point origen = new Point();
+        origen.x = 250;
+        origen.y = 250;
+        Point destino;
+        destino = getSegundoPunto(origen.x, origen.y, angulo, 140);
+        g.setColor(Color.BLACK);
+        g.drawString(hora, destino.x, destino.y);
+    }
+
+    public void marcaHora(Graphics g, int angulo) {
+        Point origen = new Point();
+        origen.x = 250;
+        origen.y = 250;
+        Point destino;
+        destino = getSegundoPunto(origen.x, origen.y, angulo, 125);
+        origen = getSegundoPunto(origen.x, origen.y, angulo, 110);
+        g.setColor(Color.BLACK);
+        g.drawLine(origen.x, origen.y, destino.x, destino.y);
+
     }
 
     public void marcaSegundero(Graphics g, int angulo) {
@@ -67,7 +106,7 @@ public class RelojAnalogico extends JFrame {
         origen.y = 250;
         Point destino;
         destino = getSegundoPunto(origen.x, origen.y, angulo, 125);
-        origen= getSegundoPunto(origen.x, origen.y, angulo, 120);
+        origen = getSegundoPunto(origen.x, origen.y, angulo, 120);
         g.setColor(Color.BLACK);
         g.drawLine(origen.x, origen.y, destino.x, destino.y);
 
@@ -91,7 +130,7 @@ public class RelojAnalogico extends JFrame {
         origen.x = 250;
         origen.y = 250;
         Point destino;
-        destino = getSegundoPunto(origen.x, origen.y, angulo, 115);
+        destino = getSegundoPunto(origen.x, origen.y, angulo, 100);
         g.setColor(color);
         g.drawLine(origen.x, origen.y, destino.x, destino.y);
     }
