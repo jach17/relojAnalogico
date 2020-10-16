130fa9b343870db14908429429187faa6569b946
Jonathan <jonathan.aldana170500@gmail.com>
2020-10-15 22:56:16 -0500

    Movimiento del segundero

--- src/relojanalogico/RelojAnalogico.java
+++ src/relojanalogico/RelojAnalogico.java
@@ -18,14 +18,13 @@ import sun.java2d.loops.DrawLine;
  */
 public class RelojAnalogico extends JFrame {
 
-    
-    public RelojAnalogico(){
-        this.setBounds(0,0, 500, 500);
+    public RelojAnalogico() {
+        this.setBounds(0, 0, 500, 500);
         this.setLocationRelativeTo(null);
     }
-    
+
     public static void main(String[] args) {
-       
+
         EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
@@ -42,44 +41,48 @@ public class RelojAnalogico extends JFrame {
     public void paint(Graphics grphcs) {
         super.paint(grphcs); //To change body of generated methods, choose Tools | Templates.
         ciruclo(grphcs);
-        dibujaSegundero(grphcs);
+        movimientoSegundero(grphcs);
     }
-    
-    public void ciruclo(Graphics g){
+
+    public void ciruclo(Graphics g) {
         g.setColor(Color.WHITE);
-        int x= this.getWidth()/4;
-        int y= this.getHeight()/4;
-        g.fillOval(x,y,2*x,2*y);
-        
+        int x = this.getWidth() / 4;
+        int y = this.getHeight() / 4;
+        g.fillOval(x, y, 2 * x, 2 * y);
+
     }
-    public void dibujaSegundero(Graphics g){
+
+    public void movimientoSegundero(final Graphics g) {
+        for (int i = 0; i < 360; i += 6) {
+            
+            try{
+                dibujaSegundero(g, i, Color.BLACK);
+                Thread.sleep(100);
+                dibujaSegundero(g, i, Color.WHITE);
+            }catch(InterruptedException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public void dibujaSegundero(Graphics g, int angulo, Color color) {
         Point origen = new Point();
-        origen.x=250;
-        origen.y=250;
+        origen.x = 250;
+        origen.y = 250;
         Point destino;
-        destino = getSegundoPunto(origen.x, origen.y, 90, 125);
-        g.setColor(Color.BLACK);
+        destino = getSegundoPunto(origen.x, origen.y, angulo, 125);
+        g.setColor(color);
         g.drawLine(origen.x, origen.y, destino.x, destino.y);
     }
-    
-    
-    public Point getSegundoPunto(int x1, int y1,int angulo, int distancia){
+
+    public Point getSegundoPunto(int x1, int y1, int angulo, int distancia) {
         Point p = new Point();
-        
-        double anguloRadianes = (Math.PI*angulo)/180;
-        
-        double x2 = distancia*Math.cos(anguloRadianes);
-        
-        double y2 = distancia*Math.sin(anguloRadianes);
-        p.x=(int)Math.round(x2) +x1;
-        p.y=(int)Math.round(y2) +y1;
-        
-        
-        System.out.println("X2: "+ p.x);
-        System.out.println("Y2: "+ p.y);
+        double anguloRadianes = (Math.PI * angulo) / 180;
+        double x2 = distancia * Math.cos(anguloRadianes);
+        double y2 = distancia * Math.sin(anguloRadianes);
+        p.x = (int) Math.round(x2) + x1;
+        p.y = (int) Math.round(y2) + y1;
         return p;
-        
     }
-    
-    
+
 }
